efa6f395aecdddb25937e044527ff94712b12eb4
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-03-05 11:39:06 -0300

    Create cont1.py

---
+++ conteudo/cont1.py
@@ -0,0 +1,4 @@
+#int= e para numeros inteiros
+#float=e para numeros reais ou com virgula
+#bool= e para valores logicos de verdadeiro ou falso exemplo true ou false(sempre usar letra maiuscula
+#str= para strings que sao palavras caracteres ou texto
