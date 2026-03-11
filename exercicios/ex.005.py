745b089df114c8f8f07eeb2cfe495c02de04273a
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-03-11 11:28:31 -0300

    Add input validation checks in ex.005.py

---
+++ exercicios/ex.005.py
@@ -0,0 +1,5 @@
+RESOLUÇÃO
+n = input('digite algo')
+print(n.isnumeric()) #para perguntar se é um número
+print(n.isalpha()) #para perguntar se é letra
+print(n.islower()) #para verificar se é apenas letras minusculas
