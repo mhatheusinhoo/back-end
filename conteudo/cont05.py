76c6a138ca69a9f8d0f8dae10347b8b70fe58d74
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-03-16 10:46:14 -0300

    Create cont05.py with operator precedence rules
    Add operator precedence definitions and examples in cont05.py

---
+++ conteudo/cont05.py
@@ -0,0 +1,13 @@
+#Definição: é a ordem na qual os operadores serão executados
+
+#1ª Expressões dentro de parenteses
+
+#2ª Em seguida vem as potenciações
+
+# 3ª Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro
+
+#4ª E por último a soma e a subtração
+#5+3*2==
+#3^ * 5 + 4 ^ 2 =
+#3^ * (5 + 4) ^ 2 =
+#O Python é utilizado em cálculos científicos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
