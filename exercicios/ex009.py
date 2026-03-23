fadf3adec097e529d4ff2a245a0031140e7fa27e
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-03-23 10:43:59 -0300

    Remove discount calculation from exercicios/ex009.py

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -1,8 +1,1 @@
-# Lê o preço do usuário
-preco = float(input("Digite o preço do produto: R$ "))
 
-# Calcula o desconto (Preço original * 0.95)
-# Multiplicar por 0.95 é um atalho matemático para tirar 5%
-novo_preco = preco * 0.95
-
-print(f"O preço com 5% de desconto é: R$ {novo_preco:.2f}")
