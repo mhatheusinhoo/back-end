f88558dd7d4e554e97c26f59719ed29b74636555
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-03-23 10:45:40 -0300

    Add discount calculation for product price

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -1,1 +1,6 @@
+preco = float(input('Qual é o preço do produto? R$ '))
+
+novo_preco = preco - 18.70 (preco * 0.05)
+
+print(f'O produto que custava R${preco:.2f}, na promoção com desconto de 5% vai custar R${novo_preco:.2f}')
 
