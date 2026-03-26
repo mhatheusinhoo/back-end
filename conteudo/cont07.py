a1408af668ab4ec194515ed9a6362f67f6d7cb7b
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-03-26 08:47:36 -0300

    Refactor square root calculations using imports

--- conteudo/cont07.py
+++ conteudo/cont07.py
@@ -3,3 +3,10 @@ num = int(input('Digite um número: ' ))
 raiz = math.sqrt(num)
 print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) #arredonda pra cima
 print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
+#mportando funcionalidades específicas
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, ceil(raiz))) #arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
+ 
