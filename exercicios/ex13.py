20dcb31274e418be8f13e762cd4b83ad4849bc8b
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-04-23 11:41:50 -0300

    Create ex13.py

---
+++ exercicios/ex13.py
@@ -0,0 +1,6 @@
+
+from math import hypot
+catop = float (input("Cateto oposto: "))
+catad = float (input("cateto adjacente: "))
+hi = hypot (catop, catad)
+print ("A hipotenusa vai medir {}".format(hi))
