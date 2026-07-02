a0850516116b78cd064e52e62ddca5aaec789d46
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-07-02 08:55:48 -0300

    Refactor code to sum odd multiples of 3

--- exercicios/ex047.py
+++ exercicios/ex047.py
@@ -1,8 +1,7 @@
-soma = 0
-contador = 0
-for numero in range(1, 501):
-    if numero % 2 != 0 and numero % 3 == 0:
-        soma += numero      
-        contador += 1        
-
-print(f"A soma de todos os {contador} valores solicitados é {soma}.")
+s = 0
+cont = 0
+for c in range(1,501,2):
+if c % 3 == 0:
+cont += 1 #conta as repetições
+s += c #soma os valores
+print(f"A soma de todos os {cont} valores solicitados é {s}")
