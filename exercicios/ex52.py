168ad503f14d27e6826cf3b24ee24b762a2b17ca
matheusinhoo <matheus.soares.branco@escola.pr.gov.br>
2026-07-30 08:56:07 -0300

    Correct weight comparison logic and output format
    Fix logic to correctly initialize 'maior' and 'menor' with the first input value. Update print statements to display the correct variables.

--- exercicios/ex52.py
+++ exercicios/ex52.py
@@ -2,13 +2,13 @@ maior = 0
 menor = 0
 for c in range(1,6):
     p = float(input(f"Peso da (c) pessoa em kg: "))
-    if p == 1:
-        maior = P
-        menor = P
+    if c == 1:
+        maior = c
+        menor = c
     else:
         if p > maior:
             maior = p
         elif p < menor:
-            menor = P
-print(f"O maior peso lido foi (maior)kg")
-print(f"O menor peso lido foi (menor)kg")
+            menor = c
+print(f"O maior peso lido foi {maior}kg")
+print(f"O menor peso lido foi {menor}kg")
